ac3998f90e1c0ee481e8f5be392dfea20c26ae4c
иван-ПК\иван <wanes101@ya.ru>
2016-10-07 22:37:03 +0800

    no message

---
+++ dpf.m
@@ -0,0 +1,9 @@
+function y= dpf(x)
+
+N = length(x);
+y=zeros(1,N);
+for(k = 0:1 : N-1)
+    for(n = 0:1:N-1)
+        y(1,k+1) = y(1,k+1) + x(1,n+1)*exp(-sqrt(-1)*2*pi*k*n/N);
+    end
+end
---
+++ exemple1.m
@@ -0,0 +1,8 @@
+n = 1:2000;
+dt = 1/4000;
+t=n*dt;
+x= sin(2*pi*500*t) + sin(2*pi*1500*t);
+y=dpf(x);
+fd = 1/dt;
+df = fd/2000;
+plot(0:df:1999*df,abs(y));
